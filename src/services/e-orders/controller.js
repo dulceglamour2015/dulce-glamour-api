69a927a0e9e0e75f43a74d16ed0428ebfdc34c53
Sebastian Acosta Alonso <45018754+jaggervlad@users.noreply.github.com>
2022-09-02 10:11:49 -0500

    adding pdf shipping e-order

--- src/services/e-orders/controller.js
+++ src/services/e-orders/controller.js
@@ -55,4 +55,49 @@ router.get('/eorder-pdf/:id', async (req, res) => {
   );
 });
 
+router.get('/eorder-shipping/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const order = await EOrder.findById(id);
+    const envioFile = 'eorder-' + id + '.pdf';
+    const directory = path.join('src', 'tmp', 'envios');
+
+    console.log({ id });
+    console.log({ order });
+
+    ejs.renderFile(
+      path.join(__dirname, '..', '..', 'views', 'eorderShipping.ejs'),
+      { order },
+      (error, data) => {
+        if (error) {
+          return res.send(error);
+        } else {
+          const html = data;
+          pdf
+            .create(html, {
+              directory,
+              type: 'pdf',
+              height: '29.7cm',
+              width: '21cm',
+            })
+            .toStream((error, stream) => {
+              if (error) return res.end(error.stack);
+              res.setHeader('Content-Type', 'application/pdf');
+              res.setHeader(
+                'Content-Disposition',
+                'inline; filename="' + envioFile + '"'
+              );
+              stream.pipe(
+                fs.createWriteStream(`./src/tmp/envios/${envioFile}`)
+              );
+              stream.pipe(res);
+            });
+        }
+      }
+    );
+  } catch (error) {
+    handleErrorResponse({ errorMsg: error });
+  }
+});
+
 module.exports = router;
